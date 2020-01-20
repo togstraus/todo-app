2480f2da82addc517121f788481f4ba0363fb879
Alex <togliattystraus@yandex.ru>
2020-01-20 16:49:16 +0400

    new file:   src/components/composition/Card.js 	modified:   src/screen/CompositionPatternScreen.js

---
+++ src/components/composition/Card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const style = {
+    container:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 15,
+    },
+    card: {
+        background: 'purple',
+        padding: 30,
+        borderRadius: 3,
+        boxShadow: '4px 4px 15px #9e9e9e',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+}
+
+const Card = ({ title = 'No Name', children }) => (
+    <div style={style.container}>
+        <div style={style.card}>
+            <h3>{title}</h3>
+            {children}
+        </div>
+    </div>
+);
+
+export default Card;
--- src/screen/CompositionPatternScreen.js
+++ src/screen/CompositionPatternScreen.js
@@ -1,36 +1,40 @@
 import React from 'react'
+import Card from '../components/composition/Card';
+import TodoScreen from './TodoScreen';
 
-const style = {
-    container:{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 15,
-    },
-    card: {
-        background: 'purple',
-        padding: 30,
-        borderRadius: 3,
-        boxShadow: '4px 4px 15px #9e9e9e',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    }
-}
+
 
 class CompositionPatternScreen extends React.Component{
 
-    
+    renderTodoApp(){
+        return(
+            <>
+                <h2>Todo-шка</h2>
+                <Card>
+                    <TodoScreen/>
+                </Card>                
+            </>
+        )
+    };
+
+    renderCard(){
+        return(
+            <>
+                <Card>
+                    <div>Содержимое карты</div>
+                </Card>  
+            </>
+        )
+    };
+
     render(){
         return(
-            <div style={style.container}>
-                <div style={style.card}>
-                    <h3>Заголовок</h3>
-                    <div>Содержимое</div>
-                </div>
-            </div>
+            <>
+                <this.renderTodoApp/>
+                <this.renderCard/>  
+            </>
         );
-    }
+    };
 };
 
 export default CompositionPatternScreen;
