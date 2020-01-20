863161d693956bbc4408405471a51e884635efff
Alex <togliattystraus@yandex.ru>
2020-01-20 16:22:46 +0400

    modified:   src/screen/CompositionPatternScreen.js

--- src/screen/CompositionPatternScreen.js
+++ src/screen/CompositionPatternScreen.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
+const style = {
+    container:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 15,
+    },
+    card: {
+        background: 'purple',
+        padding: 30,
+        borderRadius: 3,
+        boxShadow: '4px 4px 15px #9e9e9e',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+}
+
 class CompositionPatternScreen extends React.Component{
 
+    
     render(){
         return(
-            <div>
-                <h3>Заголовок</h3>
-                <div>Содержимое</div>
+            <div style={style.container}>
+                <div style={style.card}>
+                    <h3>Заголовок</h3>
+                    <div>Содержимое</div>
+                </div>
             </div>
         );
     }
